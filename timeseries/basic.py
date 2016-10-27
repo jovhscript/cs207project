a2867d101643064e2595354a10ee109515ea583a
Virgile Audi <vaudi@g.harvard.edu>
2016-10-27 19:32:53 -0400

    reparented and separated the abstract in other file for clarity

--- timeseries/basic.py
+++ timeseries/basic.py
@@ -3,6 +3,7 @@
 import itertools
 import reprlib
 import numpy as np
+import abstractclasses 
 
 def isNumericList(seq):
 	'''
@@ -15,7 +16,7 @@ def isNumericList(seq):
 		except:
 			return False
 
-class TimeSeries:
+class TimeSeries(abstractclasses.SizedContainerTimeSeriesInterface):
 	"""This TimeSeries class stores a single, ordered set of numerical data as a Python list."""
 	def __init__(self, values, times=None):
 		"""
@@ -69,177 +70,11 @@ class TimeSeries:
 			else:
 				self._times = range(0,len(self._values))
 			self.timeseries = list(zip(self._times, self._values))
-		
 
-	def __len__(self):
-		""" 
-		Returns the length of the TimeSeries object, which corresponds to the length of the timeseries attribute
-		"""
-		return len(self.timeseries)
-
-	def __iter__(self):
-		"""
-		Returns an Iterator over values
-		"""
-		return iter(self._values)
-
-	def itertimes(self):
-		"""
-		Returns an Iterator over times
-		"""
-		return iter(self._times)
-
-	def iteritems(self):
-		"""
-		Returns an Iterator over values, time pair
-		"""
-		return iter(self.timeseries)
-
-	def __getitem__(self, i):
-		""" 
-		Returns the ith value of the TimeSeries object
-		
-		Parameter
-		---------
-
-		i: int
-
-		Notes
-		-----
-
-		- If the user asks for the index of an item which is not contained in that list, an IndexError will be raised.
-		This is due to the fact that the underlying data structure is a Python list.
-		"""
-
-		return self._values[i]
-
-	def __setitem__(self, i, value):
-		""" 
-		Sets the value of the ith item TimeSeries object to the value `value`
-
-		Notes
-		-----
-
-		- !!!! The setitem method does not change the time associated with the ith item !!!!
-
-		- If the user asks for the index of an item which is not contained in that list, an IndexError will be raised.
-		This is due to the fact that the underlying data structure is a Python list.
-		"""
-		self._values[i] = value
-		self.timeseries[i] = (self._times[i], value)
-
-	def __repr__(self):
-		'''
-		This function returns the formal string representation of a TimeSeries object. We define the formal string
-		representations by:
-
-		Type(len=XX, timeseries=XX) 
-
-		Notes
-		-----
-
-		- If the TimeSeries contains more than 5 elements, we only print the first 5 elements.
-		'''
-		class_name = type(self).__name__
-		length = len(self.timeseries)
-		if length <= 5:
-			return '{}(len = {}; timeseries = {})'.format(class_name, length, self.timeseries)
-		else:
-			components = reprlib.repr(list(itertools.islice(self.timeseries,0,5)))
-			components = components[:components.find(']')]
-			return '{}(timeseries = {}, ...]; len = {})'.format(class_name, length, components)
-
-	def __str__(self):
-		'''
-		This function returns the informal string representation of a TimeSeries object which only correponds to the
-		string representation of the `timeseries` attribute.
-
-		Notes
-		-----
-
-		- If the TimeSeries contains more than 5 elements, we only print the first 5 elements.
-		'''
-
-		length = len(self.timeseries)
-		if length <= 5:
-			return str(self.timeseries)
-		else:
-			components = reprlib.repr(list(itertools.islice(self.timeseries,0,5)))
-			components = components[:components.find(']')]
-			return '{}, ...]'.format(components)
-
-	### Newly added Part 4
-	def __contains__(self, value):
-		return (value in self._values)
-
-	def values(self):
-		return np.array(self._values)
-
-	def itervalues(self):
-		return iter(self.values())
-
-	def times(self):
-		return np.array(self._times)
-
-	def items(self):
-		return self.timeseries
-
-	def __abs__(self):
-		return math.sqrt(sum(x * x for x in self.values()))
-
-	def __bool__(self):
-		return bool(abs(self))
-
-	def __neg__(self):
-		return TimeSeries([-x for x in self._values], [x for x in self._times])
-
-	def __pos__(self):
-		return TimeSeries(self._values, self._times)
-
-	@staticmethod
-	def _check_match_helper(self , rhs):
-		if (not self.hastime) or (not rhs.hastime):
-			raise NotImplemented
-		if not self._times==rhs._times:
-			raise ValueError(str(self)+' and '+str(rhs)+' must have the same time points')
-
-	def __add__(self, rhs):
-		if isinstance(rhs, TimeSeries):
-			TimeSeries._check_match_helper(self, rhs)
-			pairs = zip(self._values, rhs._values)
-			return TimeSeries([a + b for a, b in pairs], [x for x in self._times])
-		else:
-			raise TypeError(str(rhs)+' must be a TimeSeries instance')
-
-	def __sub__(self, rhs):
-		if isinstance(rhs, TimeSeries):
-			TimeSeries._check_match_helper(self, rhs)
-			pairs = zip(self._values, rhs._values)
-			return TimeSeries([a - b for a, b in pairs], [x for x in self._times])
-		else:
-			raise TypeError(str(rhs)+' must be a TimeSeries instance')
-
-	def __mul__(self, rhs):
-		if isinstance(rhs, TimeSeries):
-			TimeSeries._check_match_helper(self, rhs)
-			pairs = zip(self._values, rhs._values)
-			return TimeSeries([a * b for a, b in pairs], [x for x in self._times])
-		else:
-			raise TypeError(str(rhs)+' must be a TimeSeries instance')
-
-	def __eq__(self, rhs):
-		if isinstance(rhs, TimeSeries):
-			TimeSeries._check_match_helper(self, rhs)
-			pairs = zip(self._values, rhs._values)
-			return all([a==b for a, b in pairs])
-		else:
-			raise TypeError(str(rhs)+' must be a TimeSeries instance')
-
-
-class ArrayTimeSeries(TimeSeries):
+class ArrayTimeSeries(abstractclasses.SizedContainerTimeSeriesInterface):
 	def __init__(self, times, values):
 		assert isNumericList(values), "Values sequence must be only contain numerical entries"
-		self._values = np.array(values)
+		self._values = np.array([v for v in values])
 		if times:
 			assert isNumericList(times), "Time sequence must be only contain numerical entries"
 			assert all(times[i] <= times[i+1] for i in range(len(times)-1)), "Time sequence must be ordered"
