bf856e87d5f3975942ab4ef5f713d992557f98f9
fqian <fqian91@gmail.com>
2016-11-30 04:14:30 -0500

    Made major improvements to FileStorageManager (mainly get_size); added documentation and tests.

--- timeseries/FileStorageManager.py
+++ timeseries/FileStorageManager.py
@@ -19,106 +19,209 @@ class FileStorageManager(StorageManagerInterface):
 
 
 class CreateDB:
+    """
+    This class create a database to store key-TimeSeries pairs with the binary tree structure laid out in lab 10.
+    A few implementation details:
+        - both the key and the time series are converted into strings when saved in the database. We found inconsistencies
+        storing arrays and numpy arrays with lab 10's tree
+        - before converted to strings, numeric keys and TimeSeries are converted into dtype numpy.float64 to ensure format
+        matching (e.g. 1, 1.0, "1" are treated as the same keys)
+        - caching is implemented for quick "get_ts". There are three separate caching mechanisms:
+            1) most recently stored
+            2) most recently accessed
+            3) most frequently accessed, with size specified by user
+    This class is then called by FileStorageManager, which is a much cleaner display of the underlying functionalities in CreateDB
+    """
     def __init__(self, dbname, cachesize=5):
+        """
+        Parameters:
+        ----------------
+        dbname (str): the name for the database, ending in ".dbdb"
+        cachesize (int): the desired size of the cache for most frequently accessed timeseries.
+        """
         self.db = DB.connect(dbname)
         self.size_cache = cachesize
-        self.cache_lastStored = {}
-        self.cache_lastAccessed = {}
-        self.cache_mostAccessed = {}
-        self.count_accessed = {}
+        self.cache_lastStored = {} # cache to store last stored timeseries (size = 1)
+        self.cache_lastAccessed = {} # cache to store last accessed timeseries (size = 1)
+        self.cache_mostAccessed = {} # cache to store most frequently a (size = 1)
+        self.cache_mostAccessed = {} # cache to store most frequently accessed timeseries (size = cachesize)
+        self.count_accessed = {} # count of each id being accessed
 
     def store_ts(self, ts, id=None):
+        """
+        Parameters:
+        ----------------
+        ts (TimeSeries or ArrayTimeSeries): the timeseries to store. Has to be a valid SizedContainerTimeSeries
+        id (int or str): the id for which the timeseries will be paird to
 
+        Note:
+        ----------------
+        1. For more specifications of ts requirement, please see "series" for more details
+        2. If the id appears to be a number, then it is stored as a string of numpy.float64. Foe example, 1, 1.0, "1" and
+        "1.0" are all the same keys.
+        3. If a key already exist in the database, then storing a timeseries associated with that key will overwrite the
+        previously stored timeseries.
+        """
+
+        ### check if ts is a SizedContainerTimeSeries
         if not isinstance(ts, SizedContainerTimeSeriesInterface):
             raise TypeError('The input must be a valid timeseries')
 
+        ### check and process id
         if id:
-            id = str(id)
-            # if self.get_ts(id):
-            # 	print("Key exists, updating timeseries...")
-            if id[:7] == 'tsSize_':
-                raise ValueError("'tsSize_' is reserved for ids of timeseries sizes")
+            id = self._encode_id(id) # convert numeric id into string
+            try:
+                if self.get_ts(id): # if id already exist, a message will be shown
+                    print("id '{}' exists, updating its timeseries...".format(id))
+            except:
+                pass
         else:
-            # Generate id with timestamp
+            # Generate id with timestamp if the id is not provided
             id = "gen_id_{}".format(str(time.time()))
 
-        self.db.set(id, self._encode_ts(ts))
-        self.db.set('tsSize_'+id, str(len(ts)))
+        ### add the id-timeseries pair to the database
+        self.db.set(id, self._to_str(ts))
         self.db.commit()
-        self.cache_lastStored = {id: ts}
-        return id
+        self.cache_lastStored = {id: ts} # save the ts into the last stored cache
+        return id # the id needs to be returned in case of generated id
 
-    def _encode_ts(self, ts):
-        times_str = ','.join([str(t) for t in np.array(ts._times, dtype=np.float64)])
-        values_str = ','.join([str(v) for v in np.array(ts._values, dtype=np.float64)])
+    def get_ts(self, id):
+        """
+        Parameters:
+        ----------------
+        id (int or str): the id for which the desired length's timeseries was paird to
+
+        Returns:
+        ----------------
+        an timeseries instance that corresponds to the id in the database
+
+        Note:
+        ----------------
+        1. If the id appears to be a number, then it is stored as a string of numpy.float64. Foe example, 1, 1.0, "1" and
+        "1.0" are all the same keys.
+        2. If the id does not exist, then a KeyError will be raised
+        3. When get_size is called, the key will also trigger caching
+        """
+        id = self._encode_id(id) # encode the id into the standardized format
+
+        ### check if id is in each cache, from the order of last stored, last accessd to the most frequently accessed
+        if id in self.cache_lastStored:
+            ts = self.cache_lastStored[id]
+        elif id in self.cache_lastAccessed:
+            ts = self.cache_lastAccessed[id]
+        elif id in self.cache_mostAccessed:
+            ts = self.cache_mostAccessed[id]
+        else:
+            try:
+                ts_str = self.db.get(id) # if id not in the cache, then pull from db
+                ts = self._from_str(ts_str) # revert the stored string back to a timeseries instance
+            except:
+                raise KeyError("id {} not found in database".format(self._encode_id(id))) # raise an error if the id cannot be found
+
+        self.cache_lastAccessed = {id: ts} # update the last accessed cache
+        self._update_cache(id, ts) # update the most accessed cache
+        return ts
+
+    def get_size(self, id):
+        """
+        Parameters:
+        ----------------
+        id (int or str): the id for which the desired length's timeseries was paird to
+
+        Returns:
+        ----------------
+        an interger indicating the size of the timeseries the id corresponds to
+
+        Note:
+        ----------------
+        1. If the id appears to be a number, then it is stored as a string of numpy.float64. Foe example, 1, 1.0, "1" and
+        "1.0" are all the same keys.
+        2. If the id does not exist, then a KeyError will be raised
+        3. When get_size is called, the key will also trigger caching
+        """
+        return len(self.get_ts(self._encode_id(id)))
+
+    ### Helper function to update the most frequently accessed cache
+    def _update_cache(self, id, ts):
+        """
+        Parameters:
+        ----------------
+        id (int or str): the standardized id passed from get_ts
+        ts: the looked up timeseries passed from get_ts
+        """
+        if id in self.count_accessed:
+            self.count_accessed[id] += 1 # add 1 to the access count if id already exist in cache
+        else:
+            self.count_accessed[id] = 1 # initiate the access count if id is new
+
+        if len(self.cache_mostAccessed) < self.size_cache:
+            self.cache_mostAccessed[id] = ts # when the cache is not full, keep adding the timeseries for quick access
+        else:
+            id_currMin = sorted(self.cache_mostAccessed, key=lambda k: self.count_accessed[k])[0] # when cache is full, sort the id's by the access count and grab the one with lowest count
+            if self.count_accessed[id_currMin] < self.count_accessed[id]:
+                del self.cache_mostAccessed[id_currMin] # if the corresponding min count is smaller than the current count, delete the min accessed ts in the cache
+                self.cache_mostAccessed[id] = ts # and add the newest one
+
+    ### helper function to encode an id to a standardized str id
+    def _encode_id(self, id):
+        """
+        Parameter:
+        -----------------
+        id: int, float or string
+
+        Example:
+        >>> print(_encode_id(1))
+        1
+        >>> print(_encode_id(1.0))
+        1
+        >>> print(_encode_id(1.1))
+        1.1
+        >>> print(_encode_id("1"))
+        1
+        >>> print(_encode_id("1.0"))
+        1
+        """
+        try:
+            id = float(id)
+            if int(id) == id:
+                id = int(id)
+        except:
+            pass
+        return str(id)
+
+    ### helper function to encode a TimeSeries into a combination of two strings of np.float64
+    def _to_str(self, ts):
+        """
+        Parameters:
+        ----------------
+        ts: a TimeSeries instance passed from store_ts
+
+        Returns:
+        a string to be stored in the database
+        """
+        times_str = ','.join([str(t) for t in np.array(ts._times, dtype=np.float64)]) # use ',' to join times after converting to np.float64
+        values_str = ','.join([str(v) for v in np.array(ts._values, dtype=np.float64)]) # use ',' to join values after converting to np.float64
         return times_str+";"+values_str
 
+    ### helper function to decode the stored string into a TimeSeries instance
+    def _from_str(self, ts_str):
+        """
+        Parameters:
+        ----------------
+        ts_str: a string looked up from get
 
-    def _decode_ts(self, ts_str):
-        if ts_str == ";":
+        Returns:
+        ts: a TimeSeries instance
+        """
+        if ts_str == ";": # empty TimeSeries
             return series.TimeSeries([])
-        times_str, values_str = ts_str.split(';')
+        times_str, values_str = ts_str.split(';') # decode the two strings for times and values
         try:
-            times = [float(t) for t in times_str.split(',')]
+            times = [float(t) for t in times_str.split(',')] # convert the string back to the float list
         except:
             raise ValueError("Cannot convert times {} into float".format(times_str))
         try:
-            values = [float(v) for v in values_str.split(',')]
+            values = [float(v) for v in values_str.split(',')] # convert the string back to the float list
         except:
             raise ValueError("Cannot convert values {} into float".format(times_str))
         return series.TimeSeries(values=values, times=times)
-
-
-    def get_size(self, id):
-        try:
-            return int(self.db.get('tsSize_'+str(id)))
-        except KeyError:
-            return -1
-
-
-    ### - two caches
-    ###	- one for recently added time series
-    ###	- the other for most commonly "get" time series
-
-
-    def refresh_cache(self, id, ts):
-        if id in self.count_accessed:
-            self.count_accessed[id] += 1
-        else:
-            self.count_accessed[id] = 1
-
-        if len(self.cache_mostAccessed) < self.size_cache:
-            self.cache_mostAccessed[id] = ts
-        else:
-            id_currMin = sorted(self.cache_mostAccessed, key=lambda k: self.count_accessed[k])[0]
-            if self.count_accessed[id_currMin] < self.count_accessed[id]:
-                del self.cache_mostAccessed[id_currMin]
-                self.cache_mostAccessed[id] = ts
-
-    def get_ts(self, id):
-
-        id = str(id)
-
-        if id in self.cache_lastStored:
-            self.cache_lastAccessed = self.cache_lastStored
-            self.refresh_cache(id, self.cache_lastStored[id])
-            return self.cache_lastStored[id]
-        elif id in self.cache_lastAccessed:
-            self.refresh_cache(id, self.cache_lastAccessed[id])
-            return self.cache_lastAccessed[id]
-        elif id in self.cache_mostAccessed:
-            self.refresh_cache(id, self.cache_mostAccessed[id])
-            return self.cache_mostAccessed[id]
-        else:
-            try:
-                ts_str = self.db.get(id)
-            except KeyError:
-                return None
-
-
-        ts = self._decode_ts(ts_str)
-        self.cache_lastAccessed = {id: ts}
-
-        ### Refresh cache
-        self.refresh_cache(id, ts)
-        return ts
--- timeseries/FileStorageManagerTest.py
+++ timeseries/FileStorageManagerTest.py
@@ -2,13 +2,66 @@ import unittest
 import os
 from series import *
 from interfaces import *
-from FileStorageManager import CreateDB
+from FileStorageManager import *
 
-### INSTRUCTION:
-### run the following line to get the coverage of the test
-### py.test --doctest-modules  --cov --cov-report term-missing CreateDB.py test_CreateDB.py
+class test_FileStorageManager(unittest.TestCase):
 
-class DBTest(unittest.TestCase):
+    ### Setup the test database
+    def setUp(self):
+        self.testFSM = FileStorageManager('testFSM.dbdb')
+        self.ts = TimeSeries([-1, 0, 1.5, 4, 100], times=[0.5, 1, 2, 3, 10.5])
+        self.ts1 = TimeSeries([0, 1, 3.5, 5, 8], times=[0.5, 1, 2, 3, 10.5])
+        self.ts2 = TimeSeries([1, 1.5, 5], times=None)
+        self.ts3 = ArrayTimeSeries(times=[0.5, 1, 2, 3, 10.5], values=[-1, 0, 1.5, 4, 100])
+        self.ts4 = TimeSeries([])
+
+    ### Remove the database after done
+    def tearDown(self):
+        os.remove('testFSM.dbdb')
+        del self.testFSM
+        del self.ts
+        del self.ts1
+        del self.ts2
+        del self.ts3
+        del self.ts4
+
+    ### 1. basic store and get
+    def test_withKey(self):
+        ### with integer id
+        self.testFSM.store(ts=self.ts, id=1)
+        self.assertEqual(self.testFSM.get(1), self.ts)
+        self.assertEqual(self.testFSM.size(1), 5)
+        ### with string idArrayTimeSeries
+        self.testFSM.store(ts=self.ts, id="aaa")
+        self.assertEqual(self.testFSM.get("aaa"), self.ts)
+        self.assertEqual(self.testFSM.size("aaa"), 5)
+        ### with ArrayTimeSeries
+        self.testFSM.store(ts=self.ts3, id=2)
+        self.assertEqual(self.testFSM.get(2), self.ts3)
+        self.assertEqual(self.testFSM.size(2), 5)
+
+    ### 2. duplicate id
+    def test_IDAlreadyInDB(self):
+        self.testFSM.store(id=1, ts=self.ts)
+        self.testFSM.store(id=1, ts=self.ts1)
+        self.assertEquals(self.testFSM.get(1), self.ts1)
+
+    ### 3. ts is not timeseries
+    def test_inputNotTimeseries(self):
+        with self.assertRaises(TypeError):
+            self.testFSM.store(ts=["aaa", 1, 5], id=1)
+
+    ### 4. key is not found in database
+    def test_wrongKey(self):
+        self.testFSM.store(ts=self.ts, id=1)
+        with self.assertRaises(KeyError):
+            self.testFSM.get(2)
+    def test_wrongKeySize(self):
+        self.testFSM.store(ts=self.ts, id=1)
+        with self.assertRaises(KeyError):
+            self.testFSM.size(2)
+
+class test_CreateDB(unittest.TestCase):
 
     ### Setup the test database
     def setUp(self):
@@ -43,16 +96,21 @@ class DBTest(unittest.TestCase):
         self.assertEquals(self.testDB.get_ts(1), TimeSeries([-1, 0, 1.5, 4, 100], times=[0.5, 1, 2, 3, 10.5]))
         self.assertEquals(self.testDB.get_size(1), 5)
 
+        ### with same id but different format
+        self.assertEquals(self.testDB.get_ts(1), self.testDB.get_ts("1"))
+        self.assertEquals(self.testDB.get_ts(1.0), self.testDB.get_ts("1.0"))
+        self.assertEquals(self.testDB.get_ts("1.00"), self.testDB.get_ts("1.0"))
+
     ### 2. has no id
     def test_withNoID(self):
-        id_gen = self.testDB.store_ts(ts=self.ts)
+        for i in range(100):
+            id_gen = self.testDB.store_ts(ts=self.ts)
         self.assertEquals(self.testDB.get_ts(id_gen), TimeSeries([-1, 0, 1.5, 4, 100], times=[0.5, 1, 2, 3, 10.5]))
         self.assertEquals(self.testDB.get_size(id_gen), 5)
 
     ### 3. has no time
     def test_withNoTime(self):
         self.testDB.store_ts(id=1, ts=self.ts2)
-        ts_retrieved = self.testDB.get_ts(1)
         self.assertEquals(self.testDB.get_ts(1), TimeSeries([1, 1.5, 5], times=None))
         self.assertEquals(self.testDB.get_size(1), 3)
 
@@ -99,19 +157,30 @@ class DBTest(unittest.TestCase):
         self.assertEquals(self.testDB.cache_lastAccessed, {'2': self.ts1})
         self.assertEquals(self.testDB.count_accessed, {'1': 3, '2': 2})
 
+        # get_size should also trigger caching
+        self.testDB.get_size(1)
+        self.testDB.get_size(2)
+        self.assertEquals(self.testDB.cache_mostAccessed, {'1': self.ts, '2': self.ts1})
+        self.assertEquals(self.testDB.cache_lastAccessed, {'2': self.ts1})
+        self.assertEquals(self.testDB.count_accessed, {'1': 4, '2': 3})
+
+
         # After cache is full
         self.testDB.get_ts(3)
         self.testDB.get_ts(4)
         self.testDB.get_ts(4)
         self.assertEquals(self.testDB.cache_mostAccessed, {'1': self.ts, '2': self.ts1, '4': self.ts3})
         self.assertEquals(self.testDB.cache_lastAccessed, {'4': self.ts3})
-        self.assertEquals(self.testDB.count_accessed, {'1': 3, '2': 2, '3': 1, '4': 2})
-#
-# def suite():
-#     suite = unittest.TestSuite()
-#     suite.addTest(unittest.makeSuite(DBTest))
-#     return suite
-#
-# if __name__ == '__main__':
-#     runner = unittest.TextTestRunner(failfast=True)
-#     runner.run(suite())
+        self.assertEquals(self.testDB.count_accessed, {'1': 4, '2': 3, '3': 1, '4': 2})
+
+
+## encapsulate the tests into a suite and run test as I go
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(test_FileStorageManager))
+    suite.addTest(unittest.makeSuite(test_CreateDB))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner(failfast=True)
+    runner.run(suite())
