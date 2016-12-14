e6fc2a27023c6d2041ecf7e1f42be34ae3b40ddd
fqian <fqian91@gmail.com>
2016-12-14 08:11:15 -0500

    updated documentation and tests

--- timeseries/Similarity/store_gen_ts.py
+++ timeseries/Similarity/store_gen_ts.py
@@ -9,14 +9,7 @@ from sqlalchemy import create_engine
 from random import shuffle
 from sqlite3 import dbapi2 as sq3
 
-user = "cs207"
-password = "cs207password"
-host = "localhost"
-port = "5432"
-db = "ts_postgres"
-url = 'postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, db)
-
-engine = create_engine(url)
+### This script creates the StorageManager and PostGreSQL database used for the app
 
 def storeGenTS(engine):
     ### Fetch all the file names
@@ -30,6 +23,7 @@ def storeGenTS(engine):
     means = []
     stds = []
 
+    # remove the StorageManager if it exists
     try:
         os.remove('ts_storagemanager.dbdb')
     except:
@@ -39,15 +33,18 @@ def storeGenTS(engine):
     for filename in ts_names:
         with open("GeneratedTimeseries/"+filename, 'rb') as f:
             ts_content = pickle.load(f)
+            # calculate the metadata and append it to list
             ids.append(filename[10:])
             blargs.append(np.random.uniform())
             levels.append(np.random.choice(level_choices))
             means.append(sized_ts.mean(ts_content))
             stds.append(sized_ts.std(ts_content))
 
+            # store each timeseries into StorageManager
             SMTimeSeries(values=ts_content._values, times=ts_content._times, id=filename[10:], dbname="ts_storagemanager.dbdb")
     print("Success!")
 
+    # make a dataframe for MetaData
     genTS_df = pd.DataFrame()
     genTS_df["id"] = ids
     genTS_df["blarg"] = blargs
@@ -56,17 +53,18 @@ def storeGenTS(engine):
     genTS_df["std"] = stds
 
     print("Timeseries stored into PostGres")
+    # store the metadata into PostGreSQL database with to_sql
     genTS_df.to_sql("ts_postgresql", engine, if_exists="replace")
     print("Success!")
 
+if __name__ == '__main__':
+    # set up db configuration for postgresql and the engine
+    user = "cs207"
+    password = "cs207password"
+    host = "localhost"
+    port = "5432"
+    db = "ts_postgres"
+    url = 'postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, db)
+    engine = create_engine(url)
 
-storeGenTS(engine=engine)
-
-print("testcase: querying the first ts and its metadata: ")
-test_ts = SMTimeSeries().from_db(id="1", dbname="ts_storagemanager.dbdb")
-print("from storagemanager", test_ts)
-
-query = """SELECT * FROM ts_postgresql WHERE id='1';"""
-with engine.connect() as conn:
-    result = conn.execute(query)
-    print("from postgresql", result.fetchall())
+    storeGenTS(engine=engine)
